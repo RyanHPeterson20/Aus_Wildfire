b2d50a5aae82fd378953160b8f700bf660a21b36
Ryan Peterson <55967179+RyanHPeterson20@users.noreply.github.com>
2025-03-06 14:43:03 -0700

    Add files via upload

--- genlasso_func.R
+++ genlasso_func.R
@@ -35,7 +35,9 @@ cv.fusedlasso <- function(object, k =5, D){
     
     out_new <- fusedlasso(y = ytr, X = Xtr, D, gamma = gamma, minlam = min(lambda))
     
+
     b_new <- coef(out_new, lambda = lambda)$beta
+
     
     ote <- which(foldid == i) #testing id
     yte <- matrix(y[ote], length(ote), length(lambda)) #testing data
@@ -63,3 +65,44 @@ cv.fusedlasso <- function(object, k =5, D){
   class(out) = c("cv.trendfilter", "list")  
   return(out)
 }
+
+#TODO: include 
+
+cv.validation <- function() {
+  cl = match.call()
+  
+  y <- object$y
+  X <- object$X
+  D_new <- Matrix(D, sparse=TRUE)
+  n <- length(y)
+  
+  foldid <- c(0, rep(Seq(1, k), n-2)[Seq(1, n-2)], 0) #fold id
+  lambda <- object$lambda
+  
+  cvall <- matrix(0, nrow = k, ncol = length(lambda))
+  
+  for (i in Seq(1, k)) {
+    
+    otr <- which(foldid != i) #training id 
+    ntr <- length(otr) #length of training data
+    ytr <- y[otr] #training response
+    Xtr <- X[otr, ] #training predictors
+    
+    gamma <- object$gamma
+    
+    out_new <- fusedlasso(y = ytr, X = Xtr, D, gamma = gamma, minlam = min(lambda))
+    
+    b_new <- coef(out_new, lambda = lambda)$beta
+    
+    ote <- which(foldid == i) #testing id
+    yte <- matrix(y[ote], length(ote), length(lambda)) #testing data
+    Xte <- X[ote, ]
+    
+    pred_new <- Xte%*%b_new
+    cvall[i,] <- colMeans((yte-pred_new)^2) #cv mse
+    
+    
+  }
+  
+}
+
