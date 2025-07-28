fafe4554192847cae3caccf379b1c412d452b6c8
Ryan Peterson <55967179+RyanHPeterson20@users.noreply.github.com>
2025-07-28 00:26:21 -0600

    Add files via upload

---
+++ EOF_iod.R
@@ -0,0 +1,19 @@
+
+#EOF (PCA) function
+
+#parameters
+## A: weekly field data (not anomaly) in [lon, lat, time] (or [nx, ny, nt])
+## kmode: number of modes from PCA
+## amode: selects for the anomaly mode, 1 = for 52 week climatology (single year), 2 = all climate
+##  TODO: update amode and all function code for 12 month data 
+## lsm: land sea mask, 1 = ocean (valid), 0 = land (invalid)
+
+#output:
+## 
+
+
+eof_iod <- function(){
+  
+  
+  
+}
