086669edb993931f9595a6d1d61c254dd9cc1717
Ryan Peterson <55967179+RyanHPeterson20@users.noreply.github.com>
2025-08-12 10:42:00 -0600

    Add files via upload

---
+++ IODsstanom.R
@@ -0,0 +1,16 @@
+
+#R script for finding SST anomalies
+
+#load in data
+setwd("~/CO_AUS/Aus_CO-main/pIOD")
+load("sst_base.rda")
+
+#setup 
+
+
+#internal functions
+
+
+#test viz (remove or move down when done):
+
+
