2b5f55a06c68eb87c0b628730caf02e7e3fe71bf
Ryan Peterson <55967179+RyanHPeterson20@users.noreply.github.com>
2025-08-14 23:43:22 -0600

    Add files via upload

--- IODsstanom.R
+++ IODsstanom.R
@@ -13,6 +13,7 @@ suppressMessages(library(abind))
 #visualization and interpolation
 suppressMessages(library(fields))
 suppressMessages(library(colorspace))
+suppressMessages(library(RColorBrewer))
 
 #matlab function (e.g. detrend)
 suppressMessages(library(pracma))
@@ -38,64 +39,189 @@ sst.OISST.masked <- ifelse(lsm.IOD.array == 1, sst.OISST.new, NA)
 ##----Main----##
 
 #following the .ncl method for cmip5 model data
-#TODO: functionalize if this works
-A <- sst.OISST.masked #as [lon, lat, time]
+#currently only provides SON anoms
+sst.anoms <- function(A){
+    
+  A.new <- aperm(A, perm = c(3, 2, 1))  # reorder data as [time, lat, lon]
+  
+  #get dimensions from reordered array
+  nx <- dim(A.new)[3]
+  ny <- dim(A.new)[2] 
+  nt <- dim(A.new)[1]
+  
+  #get SON mean
+  nyears <- nt %/% 12
+  #reshape data array
+  A.temp <- array(A.new, dim = c(12, nyears, ny, nx))
+  A.son <- apply(A.temp[9:11, , , ], c(2,3,4), mean, na.rm = TRUE) #already at correct subset
+  rm(A.temp)
+  
+  #update time dim
+  nt <- dim(A.son)[1]
+  
+  #remove mean
+  var <- matrix(A.son, nrow = nt, ncol = ny * nx) #from earlier work on detrend
+  #get full period mean
+  var.mean <- colMeans(var) #TODO: visualize this spatially (transform to [lon, lat])
+  
+  var <- sweep(var, 2, colMeans(var), "-") #TODO: check how this might hold up with irregular spatial masks
+  
+  #de-trend
+  #TODO: figure out how to work with irregular NAs in time/space (some years have different spatial NAs)
+  test.na <- which(!is.na(var), arr.ind = TRUE) 
+  true.index <- unique(test.na[,2])
+  
+  A.resid <- matrix(NA, nrow = nt, ncol = ny*nx)
+  A.coef <- matrix(NA, nrow = 2, ncol = ny*nx)
+  
+  var.x <- seq_len(nt) #time "covariates"
+  var.y <- var[ ,true.index] #response variable (sst anoms)
+  
+  #TODO: add in an elif for linear and quadratic
+  var.fit <- lm(var.y ~ var.x)
+  
+  A.resid[,true.index] <- var.fit$residuals
+  A.coef[,true.index] <- var.fit$coefficients
+  
+  return(list(mean = var.mean,
+              anom = A.resid,
+              coef = A.coef))
+}
 
-A.new <- aperm(A, perm = c(3, 2, 1))  # reorder data as [time, lat, lon]
+#eof analysis function:
+#TODO: add in function for EOF
+sst.eof <- function(sst){
+  
+}
 
-dims <- dim(A.new) #get dimensions from reordered array
-nx <- dims[3]
-ny <- dims[2] 
-nt <- dims[1]
+#sst anomalies SON, for 
+##OISST
+sst.anom.oisst <- sst.anoms(sst.OISST.masked)
+#GODAS
+sst.anom.godas <- sst.anoms(sst.array)
+#SODA
+sst.anom.soda <- sst.anoms(sst.interp.SODA)
 
-#get SON mean
-nyears <- nt %/% 12
-#reshape data array
-A.temp <- array(A.new, dim = c(12, nyears, ny, nx))
-A.son <- apply(A.temp[9:11, , , ], c(2,3,4), mean, na.rm = TRUE) #already at correct subset
-rm(A.temp)
-
-#update time dim
-nt <- dim(A.son)[1]
-
-#remove mean
-var <- matrix(A.son, nrow = nt, ncol = ny * nx) #from earlier work on detrend
-#get full period mean
-var.mean <- colMeans(var) #TODO: visualize this spatially (transform to [lon, lat])
-
-var <- sweep(var, 2, colMeans(var), "-") #TODO: check how this might hold up with irregular spatial masks
-
-#detrend
-#TODO: test linear detrend and plot slope
-test.na <- which(!is.na(var), arr.ind = TRUE)
-
-A.resid <- matrix(NA, nrow = nt, ncol = ny*nx)
-A.coef <- matrix(NA, nrow = 2, ncol = ny*nx)
-
-#TODO: figure out how to work with irregular NAs in time/space (some years have different spatial NAs)
-true.index <- unique(test.na[,2])
-
-var.x <- seq_len(nt) #time "covariates"
-var.y <- var[ ,true.index] #response variable (sst anoms)
-
-var.fit <- lm(var.y ~ var.x)
-
-A.resid[,true.index] <- var.fit$residuals
-A.coef[,true.index] <- var.fit$coefficients
+#get SON average of SST anomalies
+sst.anom.avg <- (sst.anom.oisst$anom + sst.anom.godas$anom + sst.anom.soda$anom)/3
 
 
+#TODO: select for reduced region pIOD 
+IOD_maxLon <- 100
+IOD_minLon <- 40
+IOD_maxLat <- 5
+IOD_minLat <- -5
 
+#location range
+lon.values.IOD <- lon.values[lon.values <= IOD_maxLon & lon.values >= IOD_minLon]
+lon.range.IOD <- range(which(lon.values <= IOD_maxLon & lon.values >= IOD_minLon))
+
+lat.values.IOD <- lat.values[lat.values <= IOD_maxLat & lat.values >= IOD_minLat]
+lat.range.IOD <- range(which(lat.values <= IOD_maxLat & lat.values >= IOD_minLat))
+
+sst.anom <- array(sst.anom.avg, dim = c(nt, ny, nx)) 
+
+sst.anom.pIOD <- sst.anom[,lat.range.IOD[1]:lat.range.IOD[2],lon.range.IOD[1]:lon.range.IOD[2]]
+
+#EOF test
+kmode <- 2
+#sst.anom <- array(sst.anom.avg, dim = c(nt, ny, nx))  # from [nt, ny*nx] to [nt, ny, nx]
+sst.anom <- sst.anom.pIOD
+
+#sst.anom as [time, lat, lon]
+nt <- dim(sst.anom)[1]
+ny <- dim(sst.anom)[2]
+nx <- dim(sst.anom)[3]
+
+#TODO: look at just importing as  [nt, ny*nx]
+X <- matrix(sst.anom, nrow = nt, ncol = ny * nx)
+
+#get rid of NA's (masked locs)
+keep <- colSums(is.finite(X)) == nt
+X.new  <- X[, keep]
+
+svd.temp <- svd(X.new)
+
+#svd outputs
+U <- svd.temp$u[ ,1:kmode]
+D <- svd.temp$d[1:kmode]
+V <- svd.temp$v[ ,1:kmode]
+
+#outputs
+EOF.temp <- V #EOF spatial pattern
+PC.temp <- U %*% diag(D)
+per.temp <- D^2 / sum(svd.temp$d^2) 
+
+#TODO: double check negatives and splitting pc1 and pc2 for standarization
+PC1.std <- scale(PC.temp[,1], center = TRUE, scale = TRUE)
+PC2.std <- scale(PC.temp[,2], center = TRUE, scale = TRUE)
+PC.std <- scale(PC.temp, center = TRUE, scale = TRUE)
+
+#plot(-PC1.std, PC2.std, pch = 16, xlim = c(-2,4), ylim = c(-3, 4.5),
+#     xlab = "PC1", ylab = "PC2")
+plot(-PC.std[,1], PC.std[,2], pch = 16, xlim = c(-2,4), ylim = c(-3, 5),
+     xlab = "PC1", ylab = "PC2", col = "darkblue")
+points(-PC.std[c(13,16,25),1], PC.std[c(13,16,25),2], pch = 16, col = "firebrick")
+text(-PC.std[25,1], PC.std[25,2], "2006", pos = 4, cex = 0.9, col = "firebrick")
+text(-PC.std[16,1], PC.std[16,2], "1997", pos = 3, cex = 0.9, col = "firebrick")
+text(-PC.std[13,1], PC.std[13,2], "1994", pos = 4, cex = 0.9, col = "firebrick")
+points(-PC.std[c(1,6,34),1], PC.std[c(1,6,34),2], pch = 16, col = "darkgreen")
+text(-PC.std[34,1], PC.std[34,2], "2015", pos = 4, cex = 0.9, col = "darkgreen")
+text(-PC.std[6,1], PC.std[6,2], "1987", pos = 4, cex = 0.9, col = "darkgreen")
+text(-PC.std[1,1], PC.std[1,2], "1982", pos = 3, cex = 0.9, col = "darkgreen")
+abline(h = 0.5, lty = 2)
+abline(v = 1, lty = 2)
+
+#add in index plots 
+
+#finalize the spatial eof (pca)
+np <- nx*ny
+V_eof <- matrix(NA, nrow = np, ncol = kmode)
+
+#add in lsm sea data
+V_eof[keep, ] <- EOF.temp
+#
+temp <- array(t(V_eof), dim = c(2, ny, nx))  # t(var) is [nt, ny*nx]
+#v.eof1 <- -temp[1,,]
+#v.eof2 <- temp[2,,]
+eof.spatial <- aperm(temp, c(3, 2, 1))  # [nx, ny, nt]
+#eof.spatial <- aperm(Q_net_1, c(2, 1, 3))
+
+#setup color and range
+eof.absmax <- max(abs(eof.spatial), na.rm = TRUE)
+eof.range <- c(-eof.absmax, eof.absmax) 
+roma_col <- rev(divergingx_hcl(n = 48, palette = "RdYlBu"))
+
+brks <- seq(eof.range[1], eof.range[2], length.out = length(roma_col) + 1)
+
+image.plot(list(x = lon.values.IOD, y = rev(lat.values.IOD), z = -eof.spatial[,,1]), 
+           col = roma_col, breaks = brks, zlim = eof.range, 
+           xlab = "Lon", ylab = "Lat") #, main = paste0("OISST v2: ", format(as.Date(times[25+1]), "%B %Y")))
+#axes = FALSE)
+world(add=TRUE)
+
+
+image.plot(list(x = lon.values.IOD, y = rev(lat.values.IOD), z = eof.spatial[,,2]), 
+           col = roma_col, breaks = brks, zlim = eof.range, 
+           xlab = "Lon", ylab = "Lat") #, main = paste0("OISST v2: ", format(as.Date(times[25+1]), "%B %Y")))
+#axes = FALSE)
+world(add=TRUE)
 
 
 #Visualizations
 ## strong years 1994, 1997, 2006 (index: 13, 16, 25)
-## moderate years 1982, 1987, 2015 (index: 1, )
+## moderate years 1982, 1987, 2015 (index: 1, 6, 34)
 ## along with spatial mean, linear coeffs
 
-#array reformatting code
-#TODO: delete when done, (or move to function) 
-temp <- array(var, dim = c(nt, ny, nx))  # from [nt, ny*nx] to [nt, ny, nx]
-spat.diff <- aperm(temp, c(3, 2, 1))  # from [nt, ny, nx] to [nx, ny, nt]
+
+
+#TODO: set-up preferred spatial colors
+#match SST anomalies (deg C) from Cai et al 2021. n = 12, with most colors between (-1, 1)
+#color match may be tricky since the lower and upper bounds may not be included
+rybcol.12 <- rev(divergingx_hcl(n = 12, palette = "RdYlBu"))
+rybcol.48 <- rev(divergingx_hcl(n = 48, palette = "RdYlBu"))
+
+brks <- seq(eof.range2[1], eof.range2[2], length.out = length(roma_col) + 1)
 
 
 #Spatial Mean
@@ -111,6 +237,11 @@ world(add=TRUE)
 #Spatial detrend coefs
 temp <- array(A.coef, dim = c(2, ny, nx))  # from [nt, ny*nx] to [nt, ny, nx]
 spat.coefs <- aperm(temp, c(3, 2, 1))  # from [nt, ny, nx] to [nx, ny, nt]
+
+image.plot(list(x = lon.values, y = rev(lat.values), z = spat.coefs[,,1]), 
+           col = tim.colors(256), 
+           xlab = "Lon", ylab = "Lat")
+world(add=TRUE)
 
 image.plot(list(x = lon.values, y = rev(lat.values), z = spat.coefs[,,2]), 
            col = tim.colors(256), 
@@ -132,27 +263,102 @@ world(add=TRUE)
 temp <- array(var, dim = c(nt, ny, nx))  # from [nt, ny*nx] to [nt, ny, nx]
 spat.diff <- aperm(temp, c(3, 2, 1))  # from [nt, ny, nx] to [nx, ny, nt]
 
-image.plot(list(x = lon.values, y = rev(lat.values), z = spat.diff[,,13]), 
+image.plot(list(x = lon.values, y = rev(lat.values), z = spat.diff[,,16]), 
            col = tim.colors(256), 
            xlab = "Lon", ylab = "Lat")#, main = paste0("OISST v2: ", format(as.Date(times[25+1]), "%B %Y")))
 world(add=TRUE)
 
 #1993 SON anom detrended
-
-temp <- array(A.resid, dim = c(nt, ny, nx))  # from [nt, ny*nx] to [nt, ny, nx]
+k <- 13
+temp <- array(sst.anom.avg, dim = c(nt, ny, nx))  # from [nt, ny*nx] to [nt, ny, nx]
 spat.resid <- aperm(temp, c(3, 2, 1))  # from [nt, ny, nx] to [nx, ny, nt]
 
-image.plot(list(x = lon.values, y = rev(lat.values), z = spat.resid[,,13]), 
-           col = tim.colors(256), 
-           xlab = "Lon", ylab = "Lat")#, main = paste0("OISST v2: ", format(as.Date(times[25+1]), "%B %Y")))
+resid.absmax <- max(abs(spat.resid[,,k]), na.rm = TRUE)
+resid.range <- c(-resid.absmax, resid.absmax) 
+#default breaks
+brks <- seq(resid.range[1], resid.range[2], length.out = length(rybcol.48) + 1)
+#dumb color attempt
+cols <- colorRampPalette(brewer.pal(11, "RdYlBu"))(12)
+breaks.dumb <- c(resid.range[1], seq(-1,1, 0.2), resid.range[2])
+
+image.plot(list(x = lon.values, y = rev(lat.values), z = spat.resid[,,k]), 
+           col = rev(cols), breaks = breaks.dumb, zlim = resid.range, 
+           xlab = "Lon", ylab = "Lat", #, main = paste0("OISST v2: ", format(as.Date(times[25+1]), "%B %Y")))
+           axes = FALSE)
+axis(1, at = c(60, 90, 120))  
+axis(2)                      
+box()
+rect(40, -5, 100, 5, border = "black")
+world(add=TRUE)
+
+#just for iod domain
+k <- 13
+pIOD.resid <- aperm(sst.anom.pIOD, c(3, 2, 1)) 
+
+image.plot(list(x = lon.values.IOD, y = rev(lat.values.IOD), z = pIOD.resid[,,k]), 
+           col = rev(cols), breaks = breaks.dumb, zlim = resid.range, 
+           xlab = "Lon", ylab = "Lat") #, main = paste0("OISST v2: ", format(as.Date(times[25+1]), "%B %Y")))
+           #axes = FALSE)
 world(add=TRUE)
 
 
 
-
-
 ##------TEST SECTION-----##
+##test anomalies
+#TODO: functionalize if this works
+A <- sst.array #as [lon, lat, time]
+
+A.new <- aperm(A, perm = c(3, 2, 1))  # reorder data as [time, lat, lon]
+
+#get dimensions from reordered array
+nx <- dim(A.new)[3]
+ny <- dim(A.new)[2] 
+nt <- dim(A.new)[1]
+
+
+#get SON mean
+nyears <- nt %/% 12
+#reshape data array
+A.temp <- array(A.new, dim = c(12, nyears, ny, nx))
+A.son <- apply(A.temp[9:11, , , ], c(2,3,4), mean, na.rm = TRUE) #already at correct subset
+rm(A.temp)
+
+#update time dim
+nt <- dim(A.son)[1]
+
+#remove mean
+var <- matrix(A.son, nrow = nt, ncol = ny * nx) #from earlier work on detrend
+#get full period mean
+var.mean <- colMeans(var) #TODO: visualize this spatially (transform to [lon, lat])
+
+var <- sweep(var, 2, colMeans(var), "-") #TODO: check how this might hold up with irregular spatial masks
+
+#de-trend
+#TODO: test linear de-trend and plot slope
+
+#TODO: figure out how to work with irregular NAs in time/space (some years have different spatial NAs)
+test.na <- which(!is.na(var), arr.ind = TRUE) 
+true.index <- unique(test.na[,2])
+
+A.resid <- matrix(NA, nrow = nt, ncol = ny*nx)
+A.coef <- matrix(NA, nrow = 2, ncol = ny*nx)
+
+var.x <- seq_len(nt) #time "covariates"
+var.y <- var[ ,true.index] #response variable (sst anoms)
+
+var.fit <- lm(var.y ~ var.x)
+
+A.resid[,true.index] <- var.fit$residuals
+A.coef[,true.index] <- var.fit$coefficients
+
+
+
 #test viz (remove or move down when done):
+#array reformatting code
+temp <- array(var, dim = c(nt, ny, nx))  # from [nt, ny*nx] to [nt, ny, nx]
+spat.diff <- aperm(temp, c(3, 2, 1))  # from [nt, ny, nx] to [nx, ny, nt]
+
+
 #transform from [time, ny*nx] to [time, lat, lon]
 temp <- array(var, dim = c(nt, ny, nx))  # t(var) is [nt, ny*nx]
 spat.diff <- aperm(temp, c(3, 2, 1))  # [ny, nx, nt]
